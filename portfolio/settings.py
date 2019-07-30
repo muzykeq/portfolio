cd7609386982beebd6c6a7f6d75791a05ecd00ce
muzykeq <muzykeq@gmail.com>
2019-07-30 10:29:54 +0200

    Add files via upload

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,3 +1,4 @@
+from django.contrib.messages import constants as messages
 """
 Django settings for portfolio project.
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from portfolio import urls
+from django.contrib import messages
 
 # Create your views here.
 def Register(request):
 
-	return render(request, 'registration/register.html')
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			messages.success(request, f"Konto utworzono pomyślnie.")
+			login(request, user)
+			return redirect("/")
+		else:
+			for msg in form.error_messages:
+				messages.error(request, f"Wystąpił problem podczas tworzenia konta.")
+
+
+	form = UserCreationForm
+	args = {
+		'form': form
+	}
+	return render(request, 'registration/register.html', args)
